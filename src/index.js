48342ab19e636835e6370e20d39304262d34e647
Luke Morales <lukemorales@live.com>
2019-08-17 20:03:17 -0300

    Initial Setup w/ ESLint, Prettier, editorConfig and Root Import

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+
+export default function App () {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView />
+    </>
+  );
+};
