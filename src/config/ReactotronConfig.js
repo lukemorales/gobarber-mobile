524a4664628b428742e2765d18de1cf3f34cf78b
Luke Morales <lukemorales@live.com>
2019-08-18 16:26:58 -0300

    Reactotron Setup

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,15 @@
+import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
+
+if (__DEV__) {
+  const tron = Reactotron.configure({ name: 'GoBarber Mobile', host: '127.0.0.1' })
+    .useReactNative()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
