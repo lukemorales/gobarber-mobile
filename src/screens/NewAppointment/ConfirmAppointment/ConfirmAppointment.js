e444a070721fb8e7a73362c6d121218af96c8b20
Luke Morales <lukemorales@live.com>
2019-08-18 22:04:58 -0300

    feat: Confirm and Create New Appointment

--- src/screens/NewAppointment/ConfirmAppointment/ConfirmAppointment.js
+++ src/screens/NewAppointment/ConfirmAppointment/ConfirmAppointment.js
@@ -1,13 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
-import { Container } from './ConfirmAppointment_Styles';
+import React, { useState, useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import { enUS } from 'date-fns/locale';
+import { Platform, StatusBar } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function ConfirmAppointment() {
+import { Container, Avatar, Name, Time, SubmitButton } from './ConfirmAppointment_Styles';
+
+import api from '~/services/api';
+
+export default function ConfirmAppointment({ navigation }) {
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const dateFormatted = useMemo(() => formatRelative(parseISO(time), new Date(), { locale: enUS }), [time]);
+
+  async function handleCreateAppointment() {
+    await api.post(`appointments`, {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    navigation.navigate('Dashboard');
+  }
+
   return (
     <Container>
-      <View>
-        <Text>ConfirmAppointment Component</Text>
-      </View>
+      <Avatar
+        source={{
+          uri: provider.avatar
+            ? provider.avatar.url
+            : `https://ui-avatars.com/api/?background=7159c1&color=fff&name=${provider.name}`,
+        }}
+      />
+
+      <Name>{provider.name}</Name>
+      <Time>{dateFormatted}</Time>
+
+      <SubmitButton onPress={handleCreateAppointment}>Confirm Appointment</SubmitButton>
     </Container>
   );
 }
+
+ConfirmAppointment.navigationOptions = ({ navigation }) => ({
+  title: 'This is Your Appointment',
+  headerStyle: {
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight + 8 : 0,
+  },
+  headerLeft: () => (
+    <TouchableOpacity>
+      <Icon name="chevron-left" size={20} color="#fff" onPress={() => navigation.goBack()} />
+    </TouchableOpacity>
+  ),
+});
